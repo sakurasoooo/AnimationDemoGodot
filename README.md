2b52b18f3ac97f08da274732de32e9176506078d
Ying <69280095+sakurasoooo@users.noreply.github.com>
2023-02-01 19:18:30 -0500

    add level_one

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AnimationDemo
  
+
+# Special Thanks
+
+### Audio
+
+-ワッペン屋 http://www.hmix.net/music/r/r7.mp3
+
+-8-Bit Sound Effects Library https://freesound.org/people/LittleRobotSoundFactory/
--- Scripts/DemoLevel.gd
+++ Scripts/DemoLevel.gd
@@ -1,4 +1,24 @@
 extends Node2D
 
+onready var scenePlayer = $AnimationPlayer
+onready var choiceButtonLayer = $Choose
+
 func _ready():
+	scenePlayer.play("Start")
 	pass
+
+func play_branchA():
+	scenePlayer.play("BranchA")
+
+func play_branchB():
+	scenePlayer.play("BranchB")
+
+
+func _on_UpButton_pressed():
+	play_branchB()
+	choiceButtonLayer.hide()
+
+
+func _on_LeftButton_pressed():
+	play_branchA()
+	choiceButtonLayer.hide()
